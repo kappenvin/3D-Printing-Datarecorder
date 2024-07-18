655cbb3fb15008aaf9f8cfacc12a98924cc5ea76
AnatMarX <150144467+AnatMarX@users.noreply.github.com>
2024-07-18 23:41:13 +0200

    Update energy_all_measurements_trigger_correct.py

--- energy_all_measurements_trigger_correct.py
+++ energy_all_measurements_trigger_correct.py
@@ -280,7 +280,7 @@ if __name__ == "__main__":
                 initial_name = name
                 print("start measurements")
             
-                await asyncio.gather(
+                a, b, c, d, e = await asyncio.gather(
                     asyncio.to_thread(save_temperature, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa"),
                     asyncio.to_thread(save_images_picamera, slicer_settings_name, filename_final),
                     asyncio.to_thread(save_accelerometer, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa", 1),
@@ -288,6 +288,7 @@ if __name__ == "__main__":
                     start_saving_power_consumption(energy_consumption_sensor, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa"),
                     return_exceptions = True
                 )
+                print(a,b,c,d,exit)
 
                 print("Left the coroutines execution")
                 started_a_while_ago = True
