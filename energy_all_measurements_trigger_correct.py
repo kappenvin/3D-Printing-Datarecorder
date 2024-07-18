adfe122b6298711290ec313d0fc4c5ae35a81e69
AnatMarX <150144467+AnatMarX@users.noreply.github.com>
2024-07-18 17:02:26 +0200

    Update energy_all_measurements_trigger_correct.py

--- energy_all_measurements_trigger_correct.py
+++ energy_all_measurements_trigger_correct.py
@@ -272,14 +272,16 @@ if __name__ == "__main__":
                     # filename.gcode --> filename , .gcode
                     filename_final, _ = os.path.splitext(filename_pre)
 
-                    energy_consumption_sensor = p110.p110_device(config["sensor"]["current"]["username"],
+
+                except ValueError as e:
+                    print(e)
+                    slicer_settings_name, filename_pre, filename_final = name, name, name
+
+                energy_consumption_sensor = p110.p110_device(config["sensor"]["current"]["username"],
                                                                 config["sensor"]["current"]["password"],
                                                                 config["sensor"]["current"]["ip"],
                                                                 my_event,
                                                                 config["sensor"]["current"]["frequency"])
-
-                except ValueError:
-                    slicer_settings_name, filename_pre, filename_final = name, name, name
 
                 # clear the event so that the code runs again
                 my_event.clear()
