96f3bd1dd886cc05c152c1c774e3d717db12ad85
AnatMarX <150144467+AnatMarX@users.noreply.github.com>
2024-07-19 02:45:24 +0200

    Update energy_all_measurements_trigger_correct.py

--- energy_all_measurements_trigger_correct.py
+++ energy_all_measurements_trigger_correct.py
@@ -281,11 +281,11 @@ if __name__ == "__main__":
                 print("start measurements")
             
                 await asyncio.gather(
-                    asyncio.to_thread(save_temperature, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa"),
-                    # asyncio.to_thread(save_images_picamera, slicer_settings_name, filename_final),
-                    asyncio.to_thread(save_accelerometer, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa", 1),
-                    asyncio.to_thread(save_accelerometer, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa", 5),
-                    start_saving_power_consumption(energy_consumption_sensor, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa"),
+                    # asyncio.to_thread(save_temperature, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa"),
+                    asyncio.to_thread(save_images_picamera, slicer_settings_name, filename_final),
+                    # asyncio.to_thread(save_accelerometer, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa", 1),
+                    # asyncio.to_thread(save_accelerometer, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa", 5),
+                    # start_saving_power_consumption(energy_consumption_sensor, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa"),
                 )
 
 
