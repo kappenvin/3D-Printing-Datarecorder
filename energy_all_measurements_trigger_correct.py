7bf0ece60a18509d25aa77989f417bc93eb9a3d0
AnatMarX <150144467+AnatMarX@users.noreply.github.com>
2024-07-19 14:09:40 +0200

    Update energy_all_measurements_trigger_correct.py

--- energy_all_measurements_trigger_correct.py
+++ energy_all_measurements_trigger_correct.py
@@ -292,7 +292,7 @@ if __name__ == "__main__":
                     task2 = tg.create_task(asyncio.to_thread(save_images_picamera, slicer_settings_name, filename_final)),
                     task3 = tg.create_task(asyncio.to_thread(save_accelerometer, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa", 1)),
                     task4 = tg.create_task(asyncio.to_thread(save_accelerometer, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa", 5)),
-                    task5 = tg.create_task(start_saving_power_consumption(energy_consumption_sensor, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa"))
+                    # task5 = tg.create_task(start_saving_power_consumption(energy_consumption_sensor, slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa"))
 
                 print("Left the coroutines execution")
                 started_a_while_ago = True
