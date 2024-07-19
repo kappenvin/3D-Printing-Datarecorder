daf04f4d4bf5ef0fecca3122a7316df7195486aa
AnatMarX <150144467+AnatMarX@users.noreply.github.com>
2024-07-19 16:08:05 +0200

    testing

--- energy_all_measurements_trigger_correct.py
+++ energy_all_measurements_trigger_correct.py
@@ -5,14 +5,14 @@ import asyncio
 import concurrent.futures
 from datetime import datetime
 import os
-import qwiic_kx13x
+# import qwiic_kx13x
 import csv
-from picamera2 import Picamera2, Preview
-import board
-import neopixel_spi as neopixel
+# from picamera2 import Picamera2, Preview
+# import board
+# import neopixel_spi as neopixel
 import sys
-import board
-import adafruit_dht
+# import board
+# import adafruit_dht
 import logging
 from concurrent.futures import ThreadPoolExecutor
 import yaml  # To read the energy related code config file
@@ -59,168 +59,168 @@ def get_power_consumption_path(slicer_settings="unknown", part_name="unknown", d
     return final_path
 
 
-def save_accelerometer(slicer_settings="unknown", part_name="unknown", directory_path="/home/vincent/Documents/Data/Prusa", bus=1):
+# def save_accelerometer(slicer_settings="unknown", part_name="unknown", directory_path="/home/vincent/Documents/Data/Prusa", bus=1):
 
-    settings_directory = os.path.join(directory_path, slicer_settings)
-    # make directory Data/Anycubic/slicer_settings_standard
-    os.makedirs(settings_directory, exist_ok=True)
-    part_directory = os.path.join(settings_directory, part_name)
-    os.makedirs(part_directory, exist_ok=True)
-    final_directory = os.path.join(part_directory, "Accelerometer")
-    os.makedirs(final_directory, exist_ok=True)
-    final_path = os.path.join(
-        final_directory, f"accelerometer_data_bus{bus}.csv")
+#     settings_directory = os.path.join(directory_path, slicer_settings)
+#     # make directory Data/Anycubic/slicer_settings_standard
+#     os.makedirs(settings_directory, exist_ok=True)
+#     part_directory = os.path.join(settings_directory, part_name)
+#     os.makedirs(part_directory, exist_ok=True)
+#     final_directory = os.path.join(part_directory, "Accelerometer")
+#     os.makedirs(final_directory, exist_ok=True)
+#     final_path = os.path.join(
+#         final_directory, f"accelerometer_data_bus{bus}.csv")
 
-    myKx = qwiic_kx13x.QwiicKX134(bus=bus)
+#     myKx = qwiic_kx13x.QwiicKX134(bus=bus)
 
-    if myKx.connected == False:
-        print("The Qwiic KX13X Accelerometer device isn't connected to the system. Please check your connection",
-              file=sys.stderr)
-        return
+#     if myKx.connected == False:
+#         print("The Qwiic KX13X Accelerometer device isn't connected to the system. Please check your connection",
+#               file=sys.stderr)
+#         return
 
-    if myKx.begin():
-        print("Ready.")
-    else:
-        print("Make sure you're using the KX132 and not the KX134")
+#     if myKx.begin():
+#         print("Ready.")
+#     else:
+#         print("Make sure you're using the KX132 and not the KX134")
 
-    myKx.initialize(myKx.DEFAULT_SETTINGS)  # Load basic settings
-    myKx.set_range(myKx.KX134_RANGE8G)  # Update the range of the data output.
-    myKx.accel_control(False)
-    myKx.set_output_data_rate(11)
-    myKx.accel_control(True)
+#     myKx.initialize(myKx.DEFAULT_SETTINGS)  # Load basic settings
+#     myKx.set_range(myKx.KX134_RANGE8G)  # Update the range of the data output.
+#     myKx.accel_control(False)
+#     myKx.set_output_data_rate(11)
+#     myKx.accel_control(True)
 
-    # get the data and savae it with the microseconds to a csv file
-    with open(final_path, 'w', newline='') as file:
-        writer = csv.writer(file)
-        # Write the header
-        writer.writerow(["Acceleration_X", "Acceleration_Y",
-                        "Acceleration_Z", "Timestamp"])
-        while not my_event.is_set():
-            myKx.get_accel_data()
-            now = datetime.now()
+#     # get the data and savae it with the microseconds to a csv file
+#     with open(final_path, 'w', newline='') as file:
+#         writer = csv.writer(file)
+#         # Write the header
+#         writer.writerow(["Acceleration_X", "Acceleration_Y",
+#                         "Acceleration_Z", "Timestamp"])
+#         while not my_event.is_set():
+#             myKx.get_accel_data()
+#             now = datetime.now()
 
-            # Format datetime with milliseconds
-            formatted_datetime = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+#             # Format datetime with milliseconds
+#             formatted_datetime = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
-            accelerometer_data = [convert(myKx.kx134_accel.x), convert(
-                myKx.kx134_accel.y), convert(myKx.kx134_accel.z), formatted_datetime]
-            writer.writerow(accelerometer_data)
+#             accelerometer_data = [convert(myKx.kx134_accel.x), convert(
+#                 myKx.kx134_accel.y), convert(myKx.kx134_accel.z), formatted_datetime]
+#             writer.writerow(accelerometer_data)
 
-def save_images_picamera(slicer_settings="unknown", part_name="unknown", directory_path="/home/vincent/Documents/Data/Prusa"):
+# def save_images_picamera(slicer_settings="unknown", part_name="unknown", directory_path="/home/vincent/Documents/Data/Prusa"):
 
-    # check if the could be accessed
-    picam2 = Picamera2()
-    config = picam2.create_still_configuration(
-        main={"size": (1720, 1280)}, controls={"ExposureTime": 3000})
+#     # check if the could be accessed
+#     picam2 = Picamera2()
+#     config = picam2.create_still_configuration(
+#         main={"size": (1720, 1280)}, controls={"ExposureTime": 3000})
 
-    picam2.configure(config)
-    time.sleep(2)
-    picam2.start()
+#     picam2.configure(config)
+#     time.sleep(2)
+#     picam2.start()
 
-    pixels[1] = 0xFFFFFF
-    pixels[2] = 0xFFFFFF
-    pixels[3] = 0xFFFFFF
-    pixels[4] = 0xFFFFFF
-    pixels[5] = 0xFFFFFF
-    # pixels[6]=0xFFFFFF
-    pixels.show()
+#     pixels[1] = 0xFFFFFF
+#     pixels[2] = 0xFFFFFF
+#     pixels[3] = 0xFFFFFF
+#     pixels[4] = 0xFFFFFF
+#     pixels[5] = 0xFFFFFF
+#     # pixels[6]=0xFFFFFF
+#     pixels.show()
 
-    # make the directories
-    settings_directory = os.path.join(directory_path, slicer_settings)
-    # make directory Data/Anycubic/slicer_settings_standard
-    os.makedirs(settings_directory, exist_ok=True)
-    part_directory = os.path.join(settings_directory, part_name)
-    os.makedirs(part_directory, exist_ok=True)
-    final_directory = os.path.join(part_directory, "Images")
-    os.makedirs(final_directory, exist_ok=True)
+#     # make the directories
+#     settings_directory = os.path.join(directory_path, slicer_settings)
+#     # make directory Data/Anycubic/slicer_settings_standard
+#     os.makedirs(settings_directory, exist_ok=True)
+#     part_directory = os.path.join(settings_directory, part_name)
+#     os.makedirs(part_directory, exist_ok=True)
+#     final_directory = os.path.join(part_directory, "Images")
+#     os.makedirs(final_directory, exist_ok=True)
 
-    api_url = "http://imi-octopi01.imi.kit.edu//plugin/DisplayLayerProgress/values"
+#     api_url = "http://imi-octopi01.imi.kit.edu//plugin/DisplayLayerProgress/values"
 
-    # get the image and save them
-    while True:
-        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#     # get the image and save them
+#     while True:
+#         current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-        # save the file to the speicific directory
-        _, response = get_cotoprint_response(octoprint_server=api_url)
-        layer = response["layer"]["current"]
-        final_path = os.path.join(
-            final_directory, current_time+"layer_"+layer+".jpg")
-        picam2.capture_file(final_path)
-        time.sleep(1)
-        if my_event.is_set():
-            picam2.close()
-            pixels.fill(0)
-            pixels.show()
-            break 
+#         # save the file to the speicific directory
+#         _, response = get_cotoprint_response(octoprint_server=api_url)
+#         layer = response["layer"]["current"]
+#         final_path = os.path.join(
+#             final_directory, current_time+"layer_"+layer+".jpg")
+#         picam2.capture_file(final_path)
+#         time.sleep(1)
+#         if my_event.is_set():
+#             picam2.close()
+#             pixels.fill(0)
+#             pixels.show()
+#             break 
 
 
-def save_temperature(slicer_settings="unknown", part_name="unknown", directory_path="/home/vincent/Documents/Data/Prusa"):
+# def save_temperature(slicer_settings="unknown", part_name="unknown", directory_path="/home/vincent/Documents/Data/Prusa"):
 
-    dhtDevice = adafruit_dht.DHT22(board.D12)
+#     dhtDevice = adafruit_dht.DHT22(board.D12)
 
-    settings_directory = os.path.join(directory_path, slicer_settings)
-    # make directory Data/Anycubic/slicer_settings_standard
-    os.makedirs(settings_directory, exist_ok=True)
-    part_directory = os.path.join(settings_directory, part_name)
-    os.makedirs(part_directory, exist_ok=True)
-    final_directory = os.path.join(part_directory, "Temperature_Humidity")
-    os.makedirs(final_directory, exist_ok=True)
-    final_path = os.path.join(final_directory, "temperature_humidity.csv")
+#     settings_directory = os.path.join(directory_path, slicer_settings)
+#     # make directory Data/Anycubic/slicer_settings_standard
+#     os.makedirs(settings_directory, exist_ok=True)
+#     part_directory = os.path.join(settings_directory, part_name)
+#     os.makedirs(part_directory, exist_ok=True)
+#     final_directory = os.path.join(part_directory, "Temperature_Humidity")
+#     os.makedirs(final_directory, exist_ok=True)
+#     final_path = os.path.join(final_directory, "temperature_humidity.csv")
 
-    print(final_path)
+#     print(final_path)
 
-    with open(final_path, 'w', newline='') as file:
-        writer = csv.writer(file)
-        # Write the header
-        writer.writerow(["Timestamp", "Temperarture", "Humidity"])
-        print("writing")
+#     with open(final_path, 'w', newline='') as file:
+#         writer = csv.writer(file)
+#         # Write the header
+#         writer.writerow(["Timestamp", "Temperarture", "Humidity"])
+#         print("writing")
 
-        while True:
-            if my_event.is_set():
-                dhtDevice.exit()
-                break
-            try:
-                # Print the values to the serial port
-                temperature_c = dhtDevice.temperature
-                humidity = dhtDevice.humidity
+#         while True:
+#             if my_event.is_set():
+#                 dhtDevice.exit()
+#                 break
+#             try:
+#                 # Print the values to the serial port
+#                 temperature_c = dhtDevice.temperature
+#                 humidity = dhtDevice.humidity
 
-                print("humidity:", humidity)
-                print(final_path)
-                now = datetime.now()
+#                 print("humidity:", humidity)
+#                 print(final_path)
+#                 now = datetime.now()
 
-                # Format datetime with milliseconds
-                formatted_datetime = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+#                 # Format datetime with milliseconds
+#                 formatted_datetime = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
-                data_temp_hum = [formatted_datetime, temperature_c, humidity]
-                writer.writerow(data_temp_hum)
-                file.flush()
+#                 data_temp_hum = [formatted_datetime, temperature_c, humidity]
+#                 writer.writerow(data_temp_hum)
+#                 file.flush()
 
-            except RuntimeError as error:
-                # Errors happen fairly often, DHT's are hard to read, just keep going
-                print("Error:", error.args[0])
-                time.sleep(2)
-                continue
-            except Exception as error:
-                dhtDevice.exit()
-                print(error)
-                dhtDevice = adafruit_dht.DHT22(board.D12)
+#             except RuntimeError as error:
+#                 # Errors happen fairly often, DHT's are hard to read, just keep going
+#                 print("Error:", error.args[0])
+#                 time.sleep(2)
+#                 continue
+#             except Exception as error:
+#                 dhtDevice.exit()
+#                 print(error)
+#                 dhtDevice = adafruit_dht.DHT22(board.D12)
 
-            time.sleep(2)
+#             time.sleep(2)
 
 
 if __name__ == "__main__":
     NUM_PIXELS = 8
-    PIXEL_ORDER = neopixel.GRB
+    # PIXEL_ORDER = neopixel.GRB
     COLORS = (0xFF0000, 0x00FF00, 0x0000FF)
     DELAY = 0.1
 
-    spi = board.SPI()
+    # spi = board.SPI()
 
-    pixels = neopixel.NeoPixel_SPI(spi,
-                                   NUM_PIXELS,
-                                   pixel_order=PIXEL_ORDER,
-                                   brightness=1.0,
-                                   auto_write=False)
+    # pixels = neopixel.NeoPixel_SPI(spi,
+                                #    NUM_PIXELS,
+                                #    pixel_order=PIXEL_ORDER,
+                                #    brightness=1.0,
+                                #    auto_write=False)
 
 
 
