51f6c7c96b80b64dd5f80ecd8886105ed68f1cfa
AnatMarX <150144467+AnatMarX@users.noreply.github.com>
2024-07-19 16:16:33 +0200

    uhhh

---
+++ TestingAnatole/main.py
@@ -0,0 +1,63 @@
+"""
+PrintingParametersOptimizer - main
+
+Python program used to manage the 3D printer, its parameters
+and the data generated. 
+Trying to make something modular and
+adaptable to different needs.
+
+24/06 Anatole Martenne
+"""
+import os
+import time
+import yaml
+
+from scipy.stats import qmc
+from pyDOE3 import fracfact_by_res
+
+import tapo_p110_measurement as p110
+from octoprint_api import get_octoprint_response
+# os.chdir('PrintingParametersOptimizer')
+# print(os.getcwd())
+# Read config
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+print(config)
+# Set up data recorders
+
+# Ensure the raspberry is working
+input("Please start Raspberry recorder.\n\
+      Press enter to continue...")
+# Initialize the connection to the power measurement device's api
+energy_consumption_sensor = p110.p110_device(config["sensor"]["current"]["username"],
+                                             config["sensor"]["current"]["password"],
+                                             config["sensor"]["current"]["ip"],
+                                             config["sensor"]["current"]["frequency"])
+print("Energy consumption sensor initialized")
+print("Waiting for a print to begin...")
+
+initial_name = "start"
+while True:
+    operational, data = get_octoprint_response()
+
+    while not operational:
+        operational, data = get_octoprint_response()
+    state = data["state"]
+    name = data["job"]["file"]["name"]
+
+    # start measurement if the name changes otherwise let the measurement run
+    if name != initial_name and state == "Printing":
+        # slicer_settings_name,filename_pre=name.rsplit('_',1) # slicer_settings_standard_filename.gcode --> slicer_settings_standard , filename.gcode
+        # filename.gcode --> filename , .gcode
+        filename_final, _ = os.path.splitext(name)
+        energy_consumption_sensor.start(filename_final+".csv")
+        initial_name = name
+
+        print("Started measuring")
+
+    elif state != "Printing":
+        if energy_consumption_sensor.recording:
+            energy_consumption_sensor.stop()
+            print("Stopped measuring")
+            time.sleep(3)
+            print("Waiting for a print to begin...")
---
+++ TestingAnatole/tapo_p110_measurement.py
@@ -0,0 +1,67 @@
+import threading
+import pandas as pd
+from datetime import datetime
+import asyncio
+import csv
+from tapo import ApiClient
+
+
+class p110_device:
+    def __init__(self, tapo_username, tapo_password, ip_address, frequency=1):
+        self.frequency = frequency
+        self.interval = 1 / int(frequency)
+        self.tapo_username = tapo_username
+        self.tapo_password = tapo_password
+        self.ip_address = ip_address
+        self.stop_event = threading.Event()
+        self.loop = asyncio.new_event_loop()
+        self.recording = False
+        asyncio.set_event_loop(self.loop)
+
+    def start(self, filename):
+        """
+        Start to record data in "filename"
+        """
+        self.stop_event.clear()
+        self.t = threading.Thread(target=self.run_async, args=(filename,))
+        self.t.start()
+        self.recording = True
+        print("Energy recording started")
+
+    def stop(self):
+        self.stop_event.set()
+        try:
+            self.t.join()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        print("Energy recording stopped")
+
+    def run_async(self, filename):
+        self.loop.run_until_complete(self.capture_data(
+            self.interval, self.tapo_username, self.tapo_password, self.ip_address, filename))
+
+    async def capture_data(self, interval, tapo_username, tapo_password, ip_address, filename):
+        client = ApiClient(tapo_username, tapo_password)
+        device = await client.p110(ip_address)
+        try:
+            with open(filename, 'a', newline="") as file:
+                writer = csv.writer(file)
+                energy_usage = await device.get_energy_usage()
+                energy_data = energy_usage.to_dict()
+
+                if file.tell() == 0:  # Check if the file is empty to write the header
+                    writer.writerow(list(energy_data.keys()))
+
+                print("1 dot = 1 measurement")
+                while not self.stop_event.is_set():
+                    energy_usage = await device.get_energy_usage()
+                    energy_data = energy_usage.to_dict()
+
+                    writer.writerow(list(energy_data.values()))
+                    file.flush()
+
+                    print(".", end="", flush=True)
+
+                    await asyncio.sleep(interval)
+        except Exception as e:
+            print(f"An error occurred during data capture: {e}")
