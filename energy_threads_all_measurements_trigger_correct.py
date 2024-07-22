895d02bce6da50b9d6c1273af510c69e79a17943
AnatMarX <150144467+AnatMarX@users.noreply.github.com>
2024-07-22 12:12:29 +0200

    Update energy_threads_all_measurements_trigger_correct.py

--- energy_threads_all_measurements_trigger_correct.py
+++ energy_threads_all_measurements_trigger_correct.py
@@ -283,6 +283,9 @@ if __name__ == "__main__":
             my_event.clear()
             initial_name = name                
             power_consumption_path = get_power_consumption_path(slicer_settings_name, filename_final, "/home/vincent/Documents/Data/Prusa")
+            print(power_consumption_path)
+            print(power_consumption_path)
+            print(power_consumption_path)
             print("start measurements")
 
             # t0=threading.Thread(target = save_images_picamera,args=(slicer_settings_name,filename_final,)) # create t1 thread
